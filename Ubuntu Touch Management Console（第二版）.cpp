99c7dcdd8571957fe26620772f12c40128abc455
Evergardener <henry0927@outlook.com>
2019-07-27 21:18:35 +0800

    Update Ubuntu Touch Management Console（第二版）.cpp

--- Ubuntu Touch Management Console（第二版）.cpp
+++ Ubuntu Touch Management Console（第二版）.cpp
@@ -31,6 +31,22 @@ void file_pull(char *start, char *end) {
 	system(strcat(pull, end));
 }
 
+void get_img(char *code) {
+	char cmd[200] = "wget http://cdimage.ubports.com/anbox-images/anbox-boot-";
+	strcat(cmd, code);
+	strcat(cmd, ".img");
+	system(cmd);
+}
+
+void flash_img(char *code, char *part) {
+	char cmd[200] = "fastboot flash ";
+	strcat(cmd, part);
+	strcat(cmd, " anbox-boot-");
+	strcat(cmd, code);
+	strcat(cmd, ".img");
+	system(cmd);
+}
+
 void create_lib(char *id, char *name) {
 	cout << "正在创建 Libertine 容器 " << name << " 。需要一段时间，请勿关闭程序..." << endl;
 	char create[200] = "libertine-container-manager create -i ";
@@ -123,7 +139,7 @@ int main() {
 		strcpy(filepath1, "");
 		strcpy(filepath2, "");
 		cout << "功能列表：\n1. 查看设备的存储容量状态\n";
-		cout << "2. 复制本地文件到设备指定目录\n3. 复制设备文件到本地指定目录\n4. 安装 Android 包到设备（须已部署 Anbox）\n5. 卸载 Android 应用程序\n6. Libertine 桌面应用程序容器\n";
+		cout << "2. 复制本地文件到设备指定目录\n3. 复制设备文件到本地指定目录\n4. 安装 Android 包到设备\n5. 卸载 Android 应用程序\n6. Libertine 桌面应用程序容器\n";
 		cout << "7. 导入与导出设备相册\n8. 导入视频或音频文件\n9. 重载存储系统可读写\n10. 重启设备至刷机模式\n11. 强制关机\n12. 强制重新启动\n";
 		cout << "13. 重新启动并还原出厂设置\n14. 从刷机模式重启至 Ubuntu Touch\n15. 从刷机模式重启至 Recovery\n16. 从刷机模式刷入非官方 Recovery\n17. 访问设备终端\n18. 手动访问 ADB\n19. 重新连接设备\n20. 获取设备序列号" << endl << "--> ";
 		cin >> choice;
@@ -168,30 +184,112 @@ int main() {
 			char *apk_name;
 			int count = 0;
 			int flag;
-			cout << "本地安卓文件路径：";
-			cin.getline(filepath1, 200);
-			file_push(filepath1, save_path);
-			for (int i = 0; i < 500; i++) {
-				if (filepath1[i] == '.' && filepath1[i + 1] == 'a' && filepath1[i + 2] == 'p' && filepath1[i + 3] == 'k') {
-					for (int k = i + 3; k > 0; k--) {
-						count++;
-						if (filepath1[k] == '\\') {
-							flag = k;
+			cout << "您的设备是否已经安装了 Anbox？\n1. 是的\n2. 未安装\n--> ";
+			cin >> choice;
+			cin.get();
+			if (choice == 1) {
+instapk:
+				while(1) {
+					cout << endl << "本地安卓文件路径（输入“exit”并回车以退出）：";
+					cin.getline(filepath1, 200);
+					if (strcmp(filepath1, "exit") == 0) break;
+					file_push(filepath1, save_path);
+					for (int i = 0; i < 500; i++) {
+						if (filepath1[i] == '.' && filepath1[i + 1] == 'a' && filepath1[i + 2] == 'p' && filepath1[i + 3] == 'k') {
+							for (int k = i + 3; k > 0; k--) {
+								count++;
+								if (filepath1[k] == '\\') {
+									flag = k;
+									break;
+								}
+							}
 							break;
-						}
+						}	//找到 .apk 字样
+					}	//得到 apk 文件名
+					apk_name = new char[count];
+					apk_name[count - 1] = '\0';
+					for (int i = flag + 1, k = 0, calc = 0; calc < count; calc++, i++, k++) {
+						apk_name[k] = filepath1[i];
+					}	//得到 apk 文件名
+					cout << "APK 文件名：" << apk_name << endl;
+					strcat(save_path, "/");
+					strcat(save_path, apk_name);
+					apk_inst(save_path);
+				}
+			}
+			else if (choice == 2) {
+				while (1) {
+					cout << endl << "您是否需要现在安装 Anbox for Ubuntu？\n1. 什么是 Anbox？\n2. 好的，请为我安装。\n3. 退出安装 Anbox\n4. 前往安装 Android 应用程序\n--> ";
+					cin >> choice;
+					cin.get();
+					if (choice == 1) {
+						cout << endl << "Anbox 是 Linux 系统的 Android 模拟器。\n您在 Ubuntu Touch 上安装 Anbox，相当于在您的设备上安装了一个 Android 虚拟机。\n这使得您可以在 Ubuntu Touch 设备上安装并使用 Android 应用程序。\n如果您的设备购买自 Team Forth，您的设备将预置 Anbox，因此您可无需进行安装操作。\n\n请按回车键继续...";
+						cin.get();
+						cout << endl;
 					}
-					break;
-				}	//找到 .apk 字样
-			}	//得到 apk 文件名
-			apk_name = new char[count];
-			apk_name[count - 1] = '\0';
-			for (int i = flag + 1, k = 0, calc = 0; calc < count; calc++, i++, k++) {
-				apk_name[k] = filepath1[i];
-			}	//得到 apk 文件名
-			cout << "APK 文件名：" << apk_name << endl;
-			strcat(save_path, "/");
-			strcat(save_path, apk_name);
-			apk_inst(save_path);
+					else if (choice == 2) {
+						char codename[20];
+						char partitionname[20];
+						cout << endl << "如下设备支持部署 Anbox：\n1. Meizu Pro 5\n2. LG Nexus 5\n3. OnePlus One\n4. Fairphone 2\n5. BQ M10 HD\n6. BQ M10 FHD\n请选择您的设备：";
+						cin >> choice;
+						cin.get();
+						if (choice == 1) {
+							strcpy(codename, "turbo");
+							strcpy(partitionname, "bootimg");
+						}	// MEIZU
+						else if (choice == 2) {
+							strcpy(codename, "hammerhead");
+							strcpy(partitionname, "boot");
+						}	//NEXUS
+						else if (choice == 3) {
+							strcpy(codename, "bacon");
+							strcpy(partitionname, "boot");
+						}	//OPO
+						else if (choice == 4) {
+							strcpy(codename, "FP2");
+							strcpy(partitionname, "boot");
+						}	//FP2
+						else if (choice == 6) {
+							strcpy(codename, "frieza");
+							strcpy(partitionname, "boot");
+						}	//FHD
+						else if (choice == 5) {
+							strcpy(codename, "cooler");
+							strcpy(partitionname, "boot");
+						}	//HD
+						cout << endl << "准备进入刷机模式..." << endl;
+						adb_shell("sudo reboot -f bootloader");
+						cout << "准备下载 Anbox 镜像文件..." << endl;
+						get_img(codename);
+						cout << "准备刷入 Anbox 镜像文件..." << endl;
+						flash_img(codename, partitionname);
+						cout << "准备重启..." << endl;
+						system("fastboot reboot");
+						cout << "重启至 Ubuntu Touch 后请按回车键继续！";
+						cin.get();
+						cout << "准备挂载系统分区为可读写..." << endl;
+						adb_shell("sudo mount -o rw,remount /");
+						cout << "准备升级 Aptitude 镜像源..." << endl;
+						adb_shell("sudo apt update");
+						cout << "准备下载 Anbox..." << endl;
+						adb_shell("sudo apt install anbox-ubuntu-touch");
+						cout << "准备安装..." << endl;
+						adb_shell("anbox-tool install");
+						cout << "准备安装 Android 调试桥..." << endl;
+						adb_shell("sudo apt update");
+						adb_shell("sudo apt install android-tools-adb");
+						cout << endl;
+						cout << "Anbox 及其附属插件已经完全安装！" << endl;
+						cout << endl;
+						goto instapk;
+					}	//安装 Anbox
+					else if (choice == 3) break;
+					else if (choice == 4) {
+						cout << endl;
+						goto instapk;
+					}
+				}	// 安装 Anbox 与否
+			}
 			cout << endl;
 		}	//选项4
 		else if (choice == 5) {
