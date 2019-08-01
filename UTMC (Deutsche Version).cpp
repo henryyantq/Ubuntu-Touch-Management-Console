401a83e1aae57bb32430d79f8c30517a205a4cf5
Evergardener <henry0927@outlook.com>
2019-08-01 08:03:17 +0800

    Update UTMC (Deutsche Version).cpp

--- UTMC (Deutsche Version).cpp
+++ UTMC (Deutsche Version).cpp
@@ -1,1 +1,4 @@
 //Laden Sie hier den Quellcode des Programms hoch oder bearbeiten Sie ihn.
+
+/* Der Programmcode für diesen Bereich wird hauptsächlich von meinem Freund Frank verwaltet.
+Vielen Dank für die Unterstützung unserer Arbeit! */
